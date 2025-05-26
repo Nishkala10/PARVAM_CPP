7c4ff7b748acfc0b8b32aabbdd39088d97035531
Nishkala10 <nishkala.krishnamurthy@gmail.com>
2025-05-26 17:58:03 +0530

    All files

---
+++ evenorodd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter an integer";
+    cin>>n;
+    if(n%2==0)
+    cout<<"The integer is even";
+    else
+    cout<<"The integer is odd";
+    return 0;
+}
