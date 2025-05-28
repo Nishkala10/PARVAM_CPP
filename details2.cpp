84e0e7e305bd90dd21f64163743ba871ada458c5
Nishkala10 <nishkala.krishnamurthy@gmail.com>
2025-05-28 11:03:26 +0530

    All files

--- details2.cpp
+++ details2.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
+using namespace std;
 int main()
 {
-    std::string name,branch,college;
+    string name,branch,college;
     int year,sem;
-    std::cout<<"Enter your name";
-    std::getline(std::cin,name)
-    std::cout<<"Enter your branch";
-    std::getline(std::cin,branch);
-    std::cout<<"Enter your year";
-    std::cin>>branch;
-    std::cout<<"Enter your semester";
-    std::cin>>sem;
-    std::cout<<"Enter your college";
-    std::getline(std::cin,college);
+    cout<<"Enter your name";
+    getline(cin,name)
+    cout<<"Enter your branch";
+    getline(cin,branch);
+    cout<<"Enter your year";
+    cin>>branch;
+    cout<<"Enter your semester";
+    cin>>sem;
+    cout<<"Enter your college";
+    getline(cin,college);
     
 
 }
